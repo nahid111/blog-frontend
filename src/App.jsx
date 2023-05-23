4b1dd95a3b5133438e7bb0a46a3dce19adb91c17
Muhammad Nahid <mdnahid22@gmail.com>
2023-05-23 13:15:08 +0600

    Router outlet

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import Header from "./components/Header";
-import HomeScreen from "./screens/HomeScreen";
+import { Container } from 'react-bootstrap';
+import { Outlet } from 'react-router-dom';
+import Header from './components/Header';
 
 const App = () => {
   return (
     <>
       <Header />
-      <HomeScreen />
+      <Container className='my-2'>
+        <Outlet />
+      </Container>
     </>
   );
 };
