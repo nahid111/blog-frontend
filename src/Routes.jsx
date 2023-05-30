99b03f9a772e2c561c596bc5940b63bbc1ea630f
Muhammad Nahid <mdnahid22@gmail.com>
2023-05-30 21:11:47 +0600

    Page Not Found

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -11,18 +11,22 @@ import LoginScreen from "./screens/LoginScreen.jsx";
 import RegisterScreen from "./screens/RegisterScreen.jsx";
 import ProfileScreen from "./screens/ProfileScreen.jsx";
 import PostsScreen from "./screens/PostsScreen.jsx";
+import NotFound from "./components/NotFound.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<App />}>
-      <Route index={true} path="/" element={<HomeScreen />} />
-      <Route path="/login" element={<LoginScreen />} />
-      <Route path="/register" element={<RegisterScreen />} />
-      <Route path="/posts" element={<PostsScreen />} />
-      <Route path="" element={<PrivateRoute />}>
-        <Route path="/profile" element={<ProfileScreen />} />
+    <>
+      <Route path="/" element={<App />}>
+        <Route index={true} path="/" element={<HomeScreen />} />
+        <Route path="/login" element={<LoginScreen />} />
+        <Route path="/register" element={<RegisterScreen />} />
+        <Route path="/posts" element={<PostsScreen />} />
+        <Route path="" element={<PrivateRoute />}>
+          <Route path="/profile" element={<ProfileScreen />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
       </Route>
-    </Route>
+    </>
   )
 );
 
---
+++ src/components/NotFound.jsx
@@ -0,0 +1,14 @@
+const NotFound = () => {
+    return (
+      <>
+        <div className="bg-dark text-secondary px-4 py-5 my-5 text-center">
+          <div className="py-5">
+            <h1 className="display-5 fw-bold text-white">Page Not Found</h1>
+          </div>
+        </div>
+      </>
+    );
+  };
+  
+  export default NotFound;
+  
